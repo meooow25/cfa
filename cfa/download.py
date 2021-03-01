4a4566f5bbf61a5bf17d5a0d142f480b184186b5
Soumik Sarkar <soumiksarkar.3120@gmail.com>
2021-03-02 02:05:04 +0530

    Split download logic into manageable functions

--- cfa/download.py
+++ cfa/download.py
@@ -98,113 +98,107 @@ def standings():
         (615, 'Altitude'),
         (616, 'shankhs'),
     )
+
+    def process_contest(c, problems, rows):
+        data = []
+        for p in problems:
+            data.append(dict(
+                name=p['name'],
+                contest_start_time=c.start_time,
+                rating=p.get('rating'),
+                tags=p['tags'],
+            ))
+        rc = Problem.insert_many(data).on_conflict_ignore().execute()
+        logger.debug('%s problems added to db', rc)
+
+        data = []
+        for p in problems:
+            data.append(dict(
+                contest=c,
+                problem=Problem.get(
+                    Problem.name == p['name'], Problem.contest_start_time == c.start_time),
+                index=p['index'],
+            ))
+        rc = ContestProblem.insert_many(data).execute()
+        logger.debug('%s contest problems added to db', rc)
+
+        users_seen = set()
+        unrated_users = 0
+        data = []
+        data_pr = []
+        for r in rows:
+            party = r['party']
+            if len(party['members']) > 1:
+                continue  # Skip teams
+            handle = party['members'][0]['handle']
+            if handle in users_seen:
+                if (c.id, handle) in known_repeats:
+                    continue
+                raise Exception(c.id, handle)
+            users_seen.add(handle)
+            try:
+                user = User.get(User.handle == handle)
+            except User.DoesNotExist:
+                unrated_users += 1
+                continue
+
+            participant_type = ParticipantType[party['participantType']]
+            assert participant_type == ParticipantType.CONTESTANT
+            data.append(dict(
+                contest=c,
+                user=user,
+                participant_type=participant_type.value,
+                rank=r['rank'],
+                points=r['points'],
+                penalty=r['penalty'],
+                successful_hack_count=r['successfulHackCount'],
+                unsuccessful_hack_count=r['unsuccessfulHackCount'],
+            ))
+            for p, pr in zip(problems, r['problemResults']):
+                if pr['points'] == 0 and pr['rejectedAttemptCount'] == 0:
+                    # no attempt
+                    continue
+                data_pr.append(dict(
+                    contest=c,
+                    user=user,
+                    problem_index=p['index'],
+                    points=pr['points'],
+                    penalty=pr.get('penalty', 0),
+                    rejected_attempt_count=pr['rejectedAttemptCount'],
+                    best_submission_time_seconds=pr.get('bestSubmissionTimeSeconds', 0),
+                ))
+
+
+        rc = RanklistRow.insert_many(data).execute()
+        logger.debug('%s ranklist rows added to db', rc)
+        if unrated_users:
+            logger.debug('%s unrated users skipped', unrated_users)
+
+        rc = 0
+        for chunk in chunked(data_pr, 10000):
+            rc += ProblemResult.insert_many(chunk).execute()
+        logger.debug('%s problem result rows added to db', rc)
+
     for c in Contest.select():
         if ContestProblem.select().where(ContestProblem.contest == c).exists():
             continue
 
+        logger.info('contest %s %s', c.id, c.name)
+        try:
+            j = api_get('contest.standings?contestId=%s' % c.id)
+        except Exception as e:
+            logger.warning(e)
+            continue
+
+        problems = j['problems']
+        rows = j['rows']
+
         with models.db.atomic():
-            logger.info('contest %s %s', c.id, c.name)
-            try:
-                j = api_get('contest.standings?contestId=%s' % c.id)
-            except Exception as e:
-                logger.info(e)
-                continue
-
-            problems = j['problems']
-            rows = j['rows']
-
-            data = []
-            for p in problems:
-                data.append(dict(
-                    name=p['name'],
-                    contest_start_time=c.start_time,
-                    rating=p.get('rating'),
-                    tags=p['tags'],
-                ))
-            rc = Problem.insert_many(data).on_conflict_ignore().execute()
-            logger.debug('%s problems added to db', rc)
-
-            data = []
-            for p in problems:
-                data.append(dict(
-                    contest=c,
-                    problem=Problem.get(
-                        Problem.name == p['name'], Problem.contest_start_time == c.start_time),
-                    index=p['index'],
-                ))
-            rc = ContestProblem.insert_many(data).execute()
-            logger.debug('%s contest problems added to db', rc)
-
-            users_seen = set()
-            unrated_users = 0
-            data = []
-            data_pr = []
-            for r in rows:
-                party = r['party']
-                if len(party['members']) > 1:
-                    continue  # Skip teams
-                handle = party['members'][0]['handle']
-                if handle in users_seen:
-                    if (c.id, handle) in known_repeats:
-                        continue
-                    raise Exception(c.id, handle)
-                users_seen.add(handle)
-                try:
-                    user = User.get(User.handle == handle)
-                except User.DoesNotExist:
-                    unrated_users += 1
-                    continue
-
-                participant_type = ParticipantType[party['participantType']]
-                assert participant_type == ParticipantType.CONTESTANT
-                data.append(dict(
-                    contest=c,
-                    user=user,
-                    participant_type=participant_type.value,
-                    rank=r['rank'],
-                    points=r['points'],
-                    penalty=r['penalty'],
-                    successful_hack_count=r['successfulHackCount'],
-                    unsuccessful_hack_count=r['unsuccessfulHackCount'],
-                ))
-                for p, pr in zip(problems, r['problemResults']):
-                    if pr['points'] == 0 and pr['rejectedAttemptCount'] == 0:
-                        # no attempt
-                        continue
-                    data_pr.append(dict(
-                        contest=c,
-                        user=user,
-                        problem_index=p['index'],
-                        points=pr['points'],
-                        penalty=pr.get('penalty', 0),
-                        rejected_attempt_count=pr['rejectedAttemptCount'],
-                        best_submission_time_seconds=pr.get('bestSubmissionTimeSeconds', 0),
-                    ))
-
-
-            rc = RanklistRow.insert_many(data).execute()
-            logger.debug('%s ranklist rows added to db', rc)
-            if unrated_users:
-                logger.debug('%s unrated users skipped', unrated_users)
-
-            rc = 0
-            for chunk in chunked(data_pr, 10000):
-                rc += ProblemResult.insert_many(chunk).execute()
-            logger.debug('%s problem result rows added to db', rc)
+            process_contest(c, problems, rows)
 
 
 def hacks():
-    for c in Contest.select():
-        if Hack.select().where(Hack.contest_id == c.id).exists():
-            continue
-
-        logger.info('contest %s %s', c.id, c.name)
-        try:
-            hacks = api_get('contest.hacks?contestId=%s' % c.id)
-        except Exception as e:
-            logger.info(e)
-            continue
-
+    def process_contest(c, hacks):
         unrated_skipped = 0
         data = []
         for h in hacks:
@@ -235,6 +229,20 @@ def hacks():
         if unrated_skipped:
             logger.debug('%s hacks involving unrated users skipped', unrated_skipped)
 
+    for c in Contest.select():
+        if Hack.select().where(Hack.contest_id == c.id).exists():
+            continue
+
+        logger.info('contest %s %s', c.id, c.name)
+        try:
+            hacks = api_get('contest.hacks?contestId=%s' % c.id)
+        except Exception as e:
+            logger.warning(e)
+            continue
+
+        with models.db.atomic():
+            process_contest(c, hacks)
+
 
 def rating_changes():
     # These appear more than once in the same ranklist ¯\_(ツ)_/¯
@@ -248,17 +256,8 @@ def rating_changes():
         (615, 'elgris'),
         (615, 'mohamedazab'),
     )
-    for c in Contest.select():
-        if RatingChange.select().where(RatingChange.contest == c).exists():
-            continue
 
-        logger.info('contest %s %s', c.id, c.name)
-        try:
-            changes = api_get('contest.ratingChanges?contestId=%s' % c.id)
-        except Exception as e:
-            logger.info(e)
-            continue
-
+    def process_contest(c, changes):
         seen = set()
         data = []
         for d in changes:
@@ -280,19 +279,120 @@ def rating_changes():
         rc = RatingChange.insert_many(data).execute()
         logger.debug('%s rating changes added to db', rc)
 
+    for c in Contest.select():
+        if RatingChange.select().where(RatingChange.contest == c).exists():
+            continue
+
+        logger.info('contest %s %s', c.id, c.name)
+        try:
+            changes = api_get('contest.ratingChanges?contestId=%s' % c.id)
+        except Exception as e:
+            logger.info(e)
+            continue
+
+        with models.db.atomic():
+            process_contest(c, changes)
+
 
 def submissions(h5_path):
-    # TODO: There's a memory leak here, figure out what's going on
-
     # for efficiency
     user_map = {u.handle: u.id for u in User.select()}
     problem_map = {(p.contest_id, p.index): p.id for p in ContestProblem.select()}
+
+    def process_batch(c, subs, last_batch_ids):
+        # Subs may be repeated if someone has made a new submission since the last batch was
+        # processed. Batches are newest to oldest so continue processing ignoring the new sub, we're
+        # not skipping any sub in the middle so it's fine.
+        repeated = unrated_author = team = ghost = 0
+        cur_ids = set()
+
+        data = []
+        for s in subs:
+            id_ = s['id']
+            cur_ids.add(id_)
+            if id_ in last_batch_ids:
+                repeated += 1
+                continue
+
+            if 'verdict' not in s:
+                logger.debug('no verdict %s', s)
+                continue
+
+            party = s['author']
+            if len(party['members']) == 0:
+                ghost += 1
+                continue
+            if len(party['members']) > 1:
+                team += 1
+                continue
+
+            handle = party['members'][0]['handle']
+            try:
+                author_id = user_map[handle]
+            except KeyError:
+                unrated_author += 1
+                continue
+
+            problem_id = problem_map[(c.id, s['problem']['index'])]
+            typ = ParticipantType[party['participantType']]
+
+            # Same order as tmodels.Submission
+            data.append((
+                id_,
+                c.id,
+                problem_id,
+                author_id,
+                typ.value,
+                s['programmingLanguage'],
+                Submission.Verdict[s['verdict']].value,
+                Submission.TestSet[s['testset']].value,
+                s['passedTestCount'],
+                s['timeConsumedMillis'],
+                s['memoryConsumedBytes'],
+            ))
+
+        if data:
+            ctable.append(data)
+            ctable.flush()
+
+        logger.debug('%s submissions added to db', len(data))
+        if repeated or unrated_author or team or ghost:
+            logger.debug('%s repeated, %s unrated author, %s team, %s ghost',
+                            repeated, unrated_author, team, ghost)
+
+        return cur_ids
+
+    def do_contest(c):
+        # The amount of submission for many contests are huge enough to eat up ~2GB of memory and
+        # make the program crash on systems with very limited RAM.
+        # Try in batches
+        batch_size = 100_000
+
+        last_batch_ids = set()
+        done = 0
+        while True:
+            try:
+                subs = api_get('contest.status?contestId=%s&from=%d&count=%d' % (c.id, done + 1, batch_size))
+            except Exception as e:
+                stre = str(e)
+                if 'not found' in stre or 'not started' in stre:
+                    logger.info(e)
+                    break
+                raise e
+
+            logger.debug('got %s subs starting at %s', len(subs), done)
+
+            last_batch_ids = process_batch(c, subs, last_batch_ids)
+
+            if len(subs) < batch_size:
+                break
+            done += len(subs)
 
     with tmodels.Table.open_append(h5_path) as table:
         group = table.get_or_create_group(table.root, 'submissions')
 
         for c in Contest.select():
-            ctable = table.get_or_create_table(group, f'c{c.id}', tmodels.Submission)
+            ctable = table.get_or_create_table(group, f'contest{c.id}', tmodels.Submission)
 
             # pytables has no transactions, so we mark a table as done on completion
             if getattr(ctable.attrs, 'done', False):
@@ -301,86 +401,7 @@ def submissions(h5_path):
             ctable.remove_rows(0, ctable.nrows)
 
             logger.info('contest %s %s', c.id, c.name)
-            # The amount of submission for many contests are huge enough to eat up ~2GB of memory and
-            # make the program crash.
-            # Try in batches
-            batch_size = 100_000
-
-            done = 0
-            last_ids = set()
-            while True:
-                try:
-                    subs = api_get('contest.status?contestId=%s&from=%d&count=%d' % (c.id, done + 1, batch_size))
-                except Exception as e:
-                    stre = str(e)
-                    if 'not found' in stre or 'not started' in stre:
-                        logger.info(e)
-                        break
-                    raise e
-
-                logger.debug('got %s subs starting at %s', len(subs), done)
-
-                repeated = unrated_author = team = ghost = 0
-                cur_ids = set()
-
-                data = []
-                for s in subs:
-                    id_ = s['id']
-                    cur_ids.add(id_)
-                    if id_ in last_ids:
-                        repeated += 1
-                        continue
-
-                    if 'verdict' not in s:
-                        logger.debug('no verdict %s', s)
-                        continue
-
-                    party = s['author']
-                    if len(party['members']) == 0:
-                        ghost += 1
-                        continue
-                    if len(party['members']) > 1:
-                        team += 1
-                        continue
-
-                    handle = party['members'][0]['handle']
-                    try:
-                        author_id = user_map[handle]
-                    except KeyError:
-                        unrated_author += 1
-                        continue
-
-                    problem_id = problem_map[(c.id, s['problem']['index'])]
-                    typ = ParticipantType[party['participantType']]
-
-                    # Same order as tmodels.Submission
-                    data.append((
-                        id_,
-                        c.id,
-                        problem_id,
-                        author_id,
-                        typ.value,
-                        s['programmingLanguage'],
-                        Submission.Verdict[s['verdict']].value,
-                        Submission.TestSet[s['testset']].value,
-                        s['passedTestCount'],
-                        s['timeConsumedMillis'],
-                        s['memoryConsumedBytes'],
-                    ))
-
-                if data:
-                    ctable.append(data)
-                    ctable.flush()
-                
-                logger.debug('%s submissions added to db', len(data))
-                if repeated or unrated_author or team or ghost:
-                    logger.debug('%s repeated, %s unrated author, %s team, %s ghost',
-                                 repeated, unrated_author, team, ghost)
-
-                if len(subs) < batch_size:
-                    break
-                done += len(subs)
-                last_ids = cur_ids
+            do_contest(c)
 
             ctable.attrs.done = True
 
@@ -389,8 +410,8 @@ def move_subs_h5_to_db(h5_path):
     batch_size = 20000
 
     with tmodels.Table.open_read(h5_path) as table, models.db.atomic():
-        group = getattr(table.root, 'submissions')
-        all_tables = list(group)
+        group = table.root.submissions
+        all_tables = [table for table in group if getattr(table.attrs, 'done', False)]
 
         for ctable in tqdm(all_tables, desc='Copying subs to db', ncols=80):            
             with tqdm(total=ctable.nrows, desc=ctable.name, ncols=80, leave=False) as pbar:
